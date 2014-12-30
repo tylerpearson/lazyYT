7496228481a84e76d3c73f473acd2bfc5bde27fd
Daugilas Kakaras <daugilas@gmail.com>
2014-12-30 04:25:21 +0200

    start v1.0-alpha; License: MIT -> CC
    http://creativecommons.org/licenses/by-sa/4.0/

--- lazyYT.js
+++ lazyYT.js
@@ -1,7 +1,12 @@
-/*! LazyYT (lazy load Youtube videos plugin) - v0.3.4 - 2014-06-30
+/*!
+* lazyYT (lazy load YouTube videos)
+* v1.0-a1 - 2014-12-30
+* (CC) This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+* http://creativecommons.org/licenses/by-sa/4.0/
+* Contributors: https://github.com/tylerpearson/lazyYT/graphs/contributors || https://github.com/daugilas/lazyYT/graphs/contributors
+* 
 * Usage: <div class="lazyYT" data-youtube-id="laknj093n" ratio="16:9" data-parameters="rel=0">loading...</div>
-* Copyright (c) 2014 Tyler Pearson; Licensed MIT */
-
+*/
 
 ;(function ($) {
     'use strict';
